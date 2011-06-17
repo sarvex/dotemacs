7225337fb543ffd1d992dd8dfef3738d62702a2d
Victor Deryagin <vderyagin@gmail.com>
2011-06-17 11:04:24 +0300

    deleted unused jabber accounts

--- conf/jabber-config.el
+++ conf/jabber-config.el
@@ -1,13 +1,5 @@
 (setq jabber-account-list
-      `((,my-jabber-com-ua-jid
-         (:network-server . "jabber.com.ua")
-         (:password . ,my-jabber-com-ua-password)
-         (:disabled . t))
-        (,my-richim-org-jid
-         (:network-server . "richim.org")
-         (:password . ,my-richim-org-password)
-         (:disabled . t))
-        (,my-jabber-org-jid
+      `((,my-jabber-org-jid
          (:network-server . "jabber.org")
          (:password . ,my-jabber-org-password)
          (:disabled . nil))))
