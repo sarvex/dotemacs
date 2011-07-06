9415baa8a3631ddf5a1027614d3f72cf22876002
Victor Deryagin <vderyagin@gmail.com>
2011-07-06 11:59:47 +0300

    haskell-mode: removed unneeded eval-after-load

--- conf/haskell-config.el
+++ conf/haskell-config.el
@@ -7,13 +7,4 @@
                                      ,(make-char 'greek-iso8859-7 107))
                      nil))))))
 
-(eval-after-load 'haskell-mode
-  (quote
-   (progn
-     ;; (add-hook 'haskell-mode-hook 'pretty-lambdas-haskell)
-     (add-hook 'haskell-mode-hook (lambda () (subword-mode 1)))
-     (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-     (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-     ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-     ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-     )))
+;; (add-hook 'haskell-mode-hook 'pretty-lambdas-haskell)
