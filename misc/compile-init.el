f412a6caad7ccfc2073e5eea822d3ab1ade1bafd
Victor Deryagin <vderyagin@gmail.com>
2011-07-06 15:57:15 +0300

    fix compilation with emacs installed by el-get

--- misc/compile-init.el
+++ misc/compile-init.el
@@ -2,9 +2,13 @@
 (add-to-list 'load-path "~/.emacs.d/conf")
 (add-to-list 'load-path "~/.emacs.d/slime")
 
-(load "cedet-config")
-(load "elpa-config")
+(setq debug-on-error t)
+
+;; fix el-get-config loading
+(require 'inversion "~/.emacs.d/el-get/cedet/common/inversion")
+
 (load "el-get-config")
+(load "elpa-config")
 
 ;; Local Variables:
 ;; no-byte-compile: t
