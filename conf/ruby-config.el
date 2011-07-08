dcbce4597dd2ef377bc5ed2a98db7b953ea1d143
Victor Deryagin <vderyagin@gmail.com>
2011-07-08 14:06:23 +0300

    refactored ruby configuration

--- conf/ruby-config.el
+++ conf/ruby-config.el
@@ -1,30 +1,9 @@
-(require 'inf-ruby)
+(dolist (f '("\\.\\(rb\\|ru\\|rake\\|thor\\|gemspec\\)\\'"
+             "\\(rake\\|thor\\|guard\\|gem\\|cap\\|vagrant\\)file\\'"))
+  (add-to-list 'auto-mode-alist `(,f . ruby-mode)))
 
-(dolist (ext '("\\.ru\\'" "\\.rake\\'" "\\.thor\\'" "\\.gemspec\\'"
-               "\\(rake\\|thor\\|guard\\|gem\\|cap\\|vagrant\\)file\\'"))
-  (add-to-list 'auto-mode-alist `(,ext . ruby-mode)))
-
-(require 'ruby-electric)
 (setq ruby-electric-matching-delimeter-alist nil
       ruby-electric-expand-delimiters-list '(124))
-
-
-(defun my-ruby-mode-hook ()
-  (ruby-electric-mode t)
-  (drag-stuff-mode t)
-  (setq comment-column 42))
-
-
-;; Clear the compilation buffer between test runs.
-(eval-after-load 'ruby-compilation
-  '(progn
-    (defadvice ruby-do-run-w/compilation
-     (before kill-buffer (name cmdlist))
-     (let ((comp-buffer-name (format "*%s*" name)))
-       (when (get-buffer comp-buffer-name)
-         (with-current-buffer comp-buffer-name
-           (delete-region (point-min) (point-max))))))
-    (ad-activate 'ruby-do-run-w/compilation)))
 
 ;; Flymake setup
 (defun flymake-ruby-init ()
@@ -50,34 +29,51 @@
                    'flymake-display-err-menu-for-current-line)
     (flymake-mode t)))
 
+(eval-after-load 'ruby-mode
+  '(progn
+    (require 'ruby-electric)
+    (require 'inf-ruby)
 
-;; rsense
-;; (add-to-list 'load-path "~/.emacs.d/rsense/etc")
-;; (setq rsense-home (expand-file-name "~/.emacs.d/rsense"))
+    ;; enable flymake
+    (require 'flymake)
+    (dolist (f '(".+\\.\\(rb\\|ru\\|rake\\|thor\\|gemspec\\)\\'"
+                 "\\([Rr]ake\\|[Tt]hor\\|[Gg]\\(uard\\|em\\)\\|[Cc]ap\\|[Vv]agrant\\)file\\'"))
+      (push `(,f flymake-ruby-init) flymake-allowed-file-name-masks))
+    (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 
-(add-hook 'ruby-mode-hook 'inf-ruby-keys)
-(add-hook 'ruby-mode-hook 'my-ruby-mode-hook)
+    (define-key ruby-mode-map (kbd "<f1>") 'yari)
+    (define-key ruby-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
+    (define-key ruby-mode-map (kbd "C-<return>")
+     (lambda () (interactive) (move-end-of-line 1) (reindent-then-newline-and-indent)))
+    (define-key ruby-mode-map (kbd "M-<return>")
+     (lambda () (interactive) (previous-line 1) (move-end-of-line 1) (reindent-then-newline-and-indent)))))
 
-(define-key ruby-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
-(define-key ruby-mode-map (kbd "C-<return>")
-  (lambda () (interactive) (move-end-of-line 1) (reindent-then-newline-and-indent)))
-(define-key ruby-mode-map (kbd "M-<return>")
-  (lambda () (interactive) (previous-line 1) (move-end-of-line 1) (reindent-then-newline-and-indent)))
+(eval-after-load 'inf-ruby
+  '(progn
+    (inf-ruby-keys)
+    (define-key inf-ruby-mode-map (kbd "<f1>") 'yari)))
 
-(define-key ruby-mode-map (kbd "<f1>") 'yari)
-(define-key inf-ruby-mode-map (kbd "<f1>") 'yari)
-
-;; enable flymake
-(require 'flymake)
-(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
-(add-hook 'ruby-mode-hook 'flymake-ruby-enable)
-
-
+;; Clear the compilation buffer between test runs.
+(eval-after-load 'ruby-compilation
+  '(progn
+    (defadvice ruby-do-run-w/compilation
+     (before kill-buffer (name cmdlist))
+     (let ((comp-buffer-name (format "*%s*" name)))
+       (when (get-buffer comp-buffer-name)
+         (with-current-buffer comp-buffer-name
+           (delete-region (point-min) (point-max))))))
+    (ad-activate 'ruby-do-run-w/compilation)))
 
 (eval-after-load 'yari
   '(define-key yari-mode-map (kbd "<f1>") 'yari))
+
+(defun my-ruby-mode-hook ()
+  (ruby-electric-mode t)
+  (drag-stuff-mode t)
+  (setq comment-column 42)
+  (flymake-ruby-enable))
+
+(add-hook 'ruby-mode-hook 'my-ruby-mode-hook)
 
 
 (setq rinari-minor-mode-prefixes '("r")) ; must be set before rinari loading
