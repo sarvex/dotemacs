ffe455cc139d80973c0f373a3cb6005adf322516
Victor Deryagin <vderyagin@gmail.com>
2011-07-09 11:20:04 +0300

    lazy loading eshell

--- conf/eshell-config.el
+++ conf/eshell-config.el
@@ -1,42 +1,44 @@
-(require 'ansi-color)
-(require 'eshell)
-(require 'em-dirs)
-
 (setq eshell-directory-name "~/.emacs.d/eshell/"
       eshell-cmpl-cycle-completions nil
       eshell-save-history-on-exit t
       eshell-scroll-show-maximum-output nil
       eshell-scroll-to-bottom-on-input 'this
       eshell-scroll-to-bottom-on-output 'this
-      eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.hg\\|\\.git\\)/\\'"
+      eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.hg\\|\\.git\\|_darcs\\)/\\'"
       eshell-output-filter-functions '(eshell-handle-ansi-color
                                        eshell-handle-control-codes
                                        eshell-postoutput-scroll-to-bottom
                                        eshell-truncate-buffer
                                        eshell-watch-for-password-prompt))
 
-(defun my-eshell-mode-hook ()
-  (define-key eshell-mode-map (kbd "C-l")
-    (lambda () (interactive) (goto-char (point-max)) (recenter-top-bottom 0)))
+(eval-after-load 'esh-mode
+  '(progn
+    (require 'em-dirs)
 
-  (setq eshell-visual-commands
-        (append
-         '("mutt" "vim" "screen" "tmux" "lftp" "mc" "ipython" "bpython"
-           "telnet" "ssh" "tail" "most" "top" "htop" "iotop" "ncmpcpp"
-           "newsbeuter" "alsamixer" "atop")
-         eshell-visual-commands)))
+    (defun eshell/cds ()
+      "Change directory to the project's root."
+      (eshell/cd (locate-dominating-file default-directory "src")))
+
+    (defun eshell/find (dir &rest opts)
+      (find-dired dir (mapconcat 'identity opts " ")))
+
+    (setq eshell-visual-commands
+     (append
+      '("mutt" "vim" "screen" "tmux" "lftp" "mc" "ipython" "bpython"
+        "telnet" "ssh" "tail" "most" "top" "htop" "iotop" "ncmpcpp"
+        "newsbeuter" "alsamixer" "atop")
+      eshell-visual-commands))
+
+    (add-hook 'eshell-mode-hook
+     (lambda ()
+       (define-key eshell-mode-map (kbd "C-l")
+         (lambda () (interactive) (goto-char (point-max)) (recenter-top-bottom 0)))))))
 
 (eval-after-load 'esh-opt
   '(progn
     (require 'em-prompt)
     (require 'em-term)
     (require 'em-cmpl)
-    (setenv "PAGER" "cat")
-    (add-hook 'eshell-mode-hook 'my-eshell-mode-hook)))
+    (setenv "PAGER" "cat")))
 
-(defun eshell/cds ()
-  "Change directory to the project's root."
-  (eshell/cd (locate-dominating-file default-directory "src")))
 
-(defun eshell/find (dir &rest opts)
-  (find-dired dir (mapconcat 'identity opts " ")))
