81ad044c411c6c1d9dbdf03fc300aaf4ad068bfa
Victor Deryagin <vderyagin@gmail.com>
2011-07-10 11:37:59 +0300

    added function to display dired+ help

--- conf/bookmark-config.el
+++ conf/bookmark-config.el
@@ -9,3 +9,12 @@
       bmkp-bmenu-commands-file "~/.emacs.d/bmk-bmenu-commands.el"
       bmkp-bmenu-state-file "~/.emacs.d/bmk-bmenu-state.el"
       bmkp-bookmark-name-length-max 50)
+
+(defun bookmarks/diredp-help ()
+  (let ((bn "*dired+ help*"))
+    (if (get-buffer bn)
+        (pop-to-buffer-same-window bn)
+        (diredp-dired-plus-help)
+        (pop-to-buffer-same-window "*Help*")
+        (rename-buffer bn))
+    (delete-other-windows)))
