61f20fa8c84c4dd8b6812124f9846f835e2d6ae4
Victor Deryagin <vderyagin@gmail.com>
2011-08-15 07:45:57 +0300

    lorem: Only insert in kill-ring when called with ARG

--- conf/functions-configuration.el
+++ conf/functions-configuration.el
@@ -275,16 +275,19 @@ If ARG is non-nil also inserts result at point. Requires pwgen(1)"
              (buffer-name))))
 
 
-(defun lorem ()
-  "Insert a lorem ipsum."
-  (interactive)
-  (insert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
-          "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
-          "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
-          "aliquip ex ea commodo consequat. Duis aute irure dolor in "
-          "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
-          "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
-          "culpa qui officia deserunt mollit anim id est laborum."))
+(defun lorem (&optional arg)
+  "Insert a lorem ipsum text. With ARG olny puts it to kill ring."
+  (interactive "P")
+  (let ((text (concat "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
+                      "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
+                      "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
+                      "aliquip ex ea commodo consequat. Duis aute irure dolor in "
+                      "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
+                      "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
+                      "culpa qui officia deserunt mollit anim id est laborum.")))
+    (if arg
+        (kill-new text)
+        (insert text))))
 
 (defun recenter-top ()
   (interactive)
