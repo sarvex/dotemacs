150719144bf2544357136f4e804a781ddb4c1c80
Victor Deryagin <vderyagin@gmail.com>
2011-09-14 12:46:17 +0300

    twittering-mode configuration refined

--- conf/twitter-configuration.el
+++ conf/twitter-configuration.el
@@ -1,8 +1,10 @@
-(setq twittering-icon-mode t
-      twittering-use-master-password t
+(setq twittering-auth-method 'xauth
+      twittering-connection-type-order '(curl wget native)
+      twittering-icon-mode t
       twittering-private-info-file (expand-file-name "~/.emacs.d/misc/twittering.gpg")
-      twittering-auth-method 'xauth
-      twittering-connection-type-order '(wget curl native))
+      twittering-scroll-mode t
+      twittering-timer-interval 150
+      twittering-use-master-password t)
 
 (make-face 'my-twittering-username-face)
 (make-face 'my-twittering-metainfo-face)
@@ -11,7 +13,11 @@
       (concat
        "%i %FACE[my-twittering-username-face]{%s} (%S), %@:\n"
        "%FILL[ ]{%T}\n"
-       "%FILL[  ]{%FACE[my-twittering-metainfo-face]{from %f%L%r%R}}\n"))
+       "%FILL[  ]{%FACE[my-twittering-metainfo-face]{from %f%L%r%R}}"))
 
 (eval-after-load 'twittering-mode
-  '(add-hook 'twittering-mode-hook 'turn-on-visual-line-mode))
+  '(progn
+    (add-hook 'twittering-mode-hook 'turn-on-visual-line-mode)
+    (add-hook 'twittering-edit-mode-hook 'turn-on-visual-line-mode)
+    (define-key twittering-mode-map (kbd "RET") 'twittering-click)
+    (define-key twittering-mode-map (kbd "R") 'twittering-enter)))
