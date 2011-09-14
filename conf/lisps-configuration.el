c408749816462f73e273f9c7556412d02f8dd88b
Victor Deryagin <vderyagin@gmail.com>
2011-09-14 12:46:44 +0300

    configuration for lisp family languages refined

--- conf/lisps-configuration.el
+++ conf/lisps-configuration.el
@@ -1,5 +1,67 @@
+(add-to-list 'interpreter-mode-alist '("sbcl" . lisp-mode))
+
 (setq ielm-header "")
 
+(define-key emacs-lisp-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
+(define-key emacs-lisp-mode-map (kbd "C-c C-c") 'eval-defun)
+(define-key emacs-lisp-mode-map (kbd "M-.") 'find-function-at-point)
+(define-key emacs-lisp-mode-map (kbd "M-;") 'comment-dwim)
+(define-key lisp-interaction-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
+(define-key lisp-interaction-mode-map (kbd "C-c j") 'eval-print-last-sexp)
+(define-key lisp-interaction-mode-map (kbd "C-c C-c") 'eval-defun)
+(define-key lisp-interaction-mode-map (kbd "M-.") 'find-function-at-point)
+(define-key lisp-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
+
+(eval-after-load 'ielm
+  '(progn
+    (define-key inferior-emacs-lisp-mode-map (kbd "M-.") 'find-function-at-point)
+    (define-key inferior-emacs-lisp-mode-map (kbd "C-l") 'recenter-top)))
+
+
+;; hooks
+
+(add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "eLisp")))
+
+(add-hook 'lisp-interaction-mode-hook (lambda () (setq mode-name "Lisp-int")))
+
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (add-hook 'after-save-hook
+                      (lambda ()
+                        "If you're saving an elisp file, likely the .elc is no longer valid."
+                        (let ((bytecode  (concat buffer-file-name "c")))
+                          (when (file-exists-p bytecode)
+                            (delete-file bytecode))))
+                      nil
+                      'make-it-local)))
+
+(add-hook 'before-save-hook
+          (lambda ()
+            (when (member major-mode '(lisp-mode
+                                       emacs-lisp-mode
+                                       lisp-interaction-mode))
+              (indent-region (point-min) (point-max)))))
+
+(dolist (mode-hook '(emacs-lisp-mode-hook
+                     lisp-mode-hook
+                     scheme-mode-hook
+                     lisp-interaction-mode-hook
+                     ;; slime-repl-mode-hook
+                     ielm-mode-hook))
+  (add-hook mode-hook
+            (lambda ()
+              "Display pretty lambdas in lisp modes"
+              (font-lock-add-keywords
+               nil
+               `(("(?\\(lambda\\>\\)"
+                  (0 (progn
+                       (compose-region
+                        (match-beginning 1)
+                        (match-end 1)
+                        ,(make-char 'greek-iso8859-7 107))
+                       nil))))))))
+
+;; paredit for all lisp modes
 (dolist (mode-hook '(emacs-lisp-mode-hook
                      clojure-mode-hook
                      scheme-mode
@@ -8,68 +70,3 @@
                      slime-repl-mode-hook
                      ielm-mode-hook))
   (add-hook mode-hook (lambda () (paredit-mode t))))
-
-
-;;================================================================
-(defun pretty-lambdas ()
-  (font-lock-add-keywords
-   nil `(("(?\\(lambda\\>\\)"
-          (0 (progn
-               (compose-region (match-beginning 1)
-                               (match-end 1)
-                               ,(make-char 'greek-iso8859-7 107))
-               nil))))))
-
-(dolist (mode-hook '(emacs-lisp-mode-hook
-                     lisp-mode-hook
-                     scheme-mode-hook
-                     lisp-interaction-mode-hook
-                     ;; slime-repl-mode-hook
-                     ielm-mode-hook))
-  (add-hook mode-hook (lambda ()
-                        (pretty-lambdas))))
-;;================================================================
-
-(add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "eLisp")))
-
-(add-hook 'lisp-interaction-mode-hook (lambda () (setq mode-name "Lisp-int")))
-
-(add-to-list 'interpreter-mode-alist '("sbcl" . lisp-mode))
-
-;;
-;; Keys
-;;
-(define-key emacs-lisp-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
-(define-key lisp-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
-(define-key lisp-interaction-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
-
-(define-key emacs-lisp-mode-map (kbd "M-;") 'comment-dwim)
-
-(define-key emacs-lisp-mode-map (kbd "C-c C-c") 'eval-defun)
-
-(define-key emacs-lisp-mode-map (kbd "M-.") 'find-function-at-point)
-(define-key lisp-interaction-mode-map (kbd "M-.") 'find-function-at-point)
-
-(eval-after-load 'ielm
-  '(progn
-    (define-key inferior-emacs-lisp-mode-map (kbd "M-.") 'find-function-at-point)
-    (define-key inferior-emacs-lisp-mode-map (kbd "C-l") 'recenter-top)))
-
-(define-key lisp-interaction-mode-map (kbd "C-c j") 'eval-print-last-sexp)
-
-(defun my-remove-elc-on-save ()
-  "If you're saving an elisp file, likely the .elc is no longer valid."
-  (make-local-variable 'after-save-hook)
-  (add-hook 'after-save-hook
-            (lambda ()
-              (if (file-exists-p (concat buffer-file-name "c"))
-                  (delete-file (concat buffer-file-name "c"))))))
-
-(add-hook 'emacs-lisp-mode-hook 'my-remove-elc-on-save)
-
-(add-hook 'before-save-hook
-          (lambda ()
-            (when (member major-mode '(lisp-mode
-                                       emacs-lisp-mode
-                                       lisp-interaction-mode))
-              (indent-region (point-min) (point-max)))))
