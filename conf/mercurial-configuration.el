6d7cfe5f21f52174aa146849095f23ab3e7f5f1a
Victor Deryagin <vderyagin@gmail.com>
2011-10-04 13:21:36 +0300

    ahg-status keys setup moved to hook

--- conf/mercurial-configuration.el
+++ conf/mercurial-configuration.el
@@ -1,19 +1,24 @@
 (setq ahg-subprocess-coding-system 'utf-8
       ahg-global-key-prefix (kbd "C-c h g"))
 
+(defun my-ahg-status-hook ()
+  (define-key ahg-status-mode-map (kbd "m") 'ahg-status-toggle-mark)
+  (define-key ahg-status-mode-map (kbd "n") 'next-line)
+  (define-key ahg-status-mode-map (kbd "<SPC>") 'next-line)
+  (define-key ahg-status-mode-map (kbd "p") 'previous-line)
+  (define-key ahg-status-mode-map (kbd "P")
+    (lambda () (interactive) (ahg-do-command "push"))))
+
 (eval-after-load 'ahg
-  '(progn
+  '(add-hook 'ahg-status-mode-hook 'my-ahg-status-hook))
 
-    (define-key ahg-status-mode-map (kbd "m") 'ahg-status-toggle-mark)
-    (define-key ahg-status-mode-map (kbd "n") 'next-line)
-    (define-key ahg-status-mode-map (kbd "p") 'previous-line)
 
-    (define-key ahg-status-mode-map (kbd "P")
-     (lambda () (interactive) (ahg-do-command "push")))
+;; MQ patches:
 
-    (define-key ahg-status-mode-map (kbd "<backspace>")
-     (lambda () (interactive) (save-excursion (ahg-status-unmark)) (forward-line -1)))
+(add-to-list 'auto-mode-alist '("\\.hg/patches\\(?:-[^/]+\\)?/" . diff-mode))
 
-    ;; Works only this way for some reason
-    (add-hook 'ahg-status-mode-hook
-     (lambda () (define-key ahg-status-mode-map (kbd "<SPC>") 'next-line)))))
+(defun mq-patch-set-default-directory ()
+  (when (string= ".hg" (nth 2 (reverse (split-string default-directory "/"))))
+    (setq default-directory (expand-file-name (concat default-directory "../../")))))
+
+(add-hook 'diff-mode-hook 'mq-patch-set-default-directory)
