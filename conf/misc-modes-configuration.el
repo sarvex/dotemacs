b8b0d90dde219342040f8d92775733b6a31ccac7
Victor Deryagin <vderyagin@gmail.com>
2011-10-04 13:21:15 +0300

    simpler hook for make-mode

--- conf/misc-modes-configuration.el
+++ conf/misc-modes-configuration.el
@@ -144,14 +144,12 @@
     (define-key sh-mode-map (kbd "M-l") 'downcase-word)))
 
 
-
 (eval-after-load 'make-mode
-  '(dolist (mode-map '(makefile-automake-mode-map
-                       makefile-bsdmake-mode-map
-                       makefile-gmake-mode-map
-                       makefile-imake-mode-map
-                       makefile-makepp-mode-map))
-    (define-key mode-map (kbd "<f9>") 'recompile)))
+  '(define-key makefile-mode-map (kbd "<f9>") 'recompile))
+
+
+(eval-after-load 'compile
+  '(define-key compilation-mode-map (kbd "<f9>") 'recompile))
 
 
 (setq shell-file-name "/bin/zsh"
