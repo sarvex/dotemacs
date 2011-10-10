2d4aab5fa1da932f4daf98ce3418d14e8d879546
Victor Deryagin <vderyagin@gmail.com>
2011-10-10 07:42:03 +0300

    flyspell in magit-log-edit-mode

--- conf/git-configuration.el
+++ conf/git-configuration.el
@@ -4,6 +4,10 @@
       magit-repo-dirs-depth 6
       magit-status-buffer-switch-function 'switch-to-buffer)
 
+(defun my-magit-log-edit-hook ()
+  (turn-on-auto-fill)
+  (flyspell-mode t))
+
 (eval-after-load 'magit
   '(progn
     ;; solving conflicts with my windows-operating keys
@@ -11,7 +15,8 @@
     (define-key magit-mode-map (kbd "M-2") nil)
     (define-key magit-mode-map (kbd "M-3") nil)
     (define-key magit-mode-map (kbd "M-4") nil)
-    (add-hook 'magit-log-edit-mode-hook 'turn-on-auto-fill)))
+
+    (add-hook 'magit-log-edit-mode-hook 'my-magit-log-edit-hook)))
 
 (setq gist-authenticate-function ' gist-basic-authentication
       ;; gist-authenticate-function 'gist-oauth2-authentication
