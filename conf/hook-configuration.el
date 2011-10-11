2c11271ac11188d91f0b0e91baf4724c4657d4e6
Victor Deryagin <vderyagin@gmail.com>
2011-10-11 15:53:31 +0300

    converted hooks from lambdas to named functions (more robust)

--- conf/hook-configuration.el
+++ conf/hook-configuration.el
@@ -87,38 +87,50 @@
 
   ;; hl-line
   (dolist (hook hl-line-modes-hooks)
-    (add-hook hook (lambda () (hl-line-mode t))))
+    (add-hook hook 'my-turn-on-hl-line-mode))
 
   ;; automatic matching paren insertion
   (dolist (hook autopair-modes-hooks)
-    (add-hook hook (lambda () (autopair-mode t))))
+    (add-hook hook 'my-turn-on-autopair-mode))
 
   ;; whitespaces
   (dolist (hook whitespace-modes-hooks)
-    (add-hook hook (lambda () (whitespace-mode t))))
+    (add-hook hook 'my-turn-on-whitespace-mode))
 
   ;; highlight keywords
   (dolist (hook hl-keywords-modes-hooks)
-    (add-hook hook
-              (lambda ()
-                (font-lock-add-keywords
-                 nil
-                 '(("\\<\\(XXX\\|TODO\\|NOTE\\|BUG\\|FIXME\\|WARNING\\|HACK\\|REFACTOR\\|IMPORTANT\\|HERE BE DRAGONS\\):"
-                    1 font-lock-warning-face t))))))
+    (add-hook hook 'my-turn-on-hl-keywords))
 
   ;; eldoc
   (dolist (hook eldoc-modes-hooks)
     (add-hook hook 'turn-on-eldoc-mode)))
 
-(add-hook 'before-save-hook
-          (lambda () (delete-trailing-whitespace)))
+(defun my-turn-on-hl-line-mode ()
+  (hl-line-mode t))
+
+(defun my-turn-on-autopair-mode ()
+  (autopair-mode t))
+
+(defun my-turn-on-whitespace-mode ()
+  (whitespace-mode t))
+
+(defun my-turn-on-hl-keywords ()
+  (font-lock-add-keywords
+   nil
+   '(("\\<\\(XXX\\|TODO\\|NOTE\\|BUG\\|FIXME\\|WARNING\\|HACK\\|REFACTOR\\|IMPORTANT\\|HERE BE DRAGONS\\):"
+      1 font-lock-warning-face t))))
+
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
 (add-hook 'comint-output-filter-functions 'comint-truncate-buffer)
 
 (add-hook 'after-save-hook 'desktop-save-in-desktop-dir)
 
-(add-hook 'term-mode-hook (lambda () (setq word-wrap nil)))
+(add-hook 'term-mode-hook 'my-turn-off-word-wrap)
+(defun my-turn-off-word-wrap ()
+  (setq word-wrap nil))
 
 ;; (add-hook 'text-mode-hook 'turn-on-orgstruct++)
 ;; (add-hook 'text-mode-hook 'turn-on-orgtbl)
@@ -126,10 +138,10 @@
 
 (add-hook 'help-mode-hook 'turn-on-visual-line-mode)
 
-(add-hook 'find-file-hook
-          (lambda ()
-            "Automatically create file's parent path if it does not exist."
-            (let ((dir (file-name-directory buffer-file-name)))
-              (when (and (not (file-exists-p dir))
-                         (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
-                (make-directory dir t)))))
+(add-hook 'find-file-hook 'my-create-parent-directories-for-file)
+(defun my-create-parent-directories-for-file ()
+  "Automatically create file's parent path if it does not exist."
+  (let ((dir (file-name-directory buffer-file-name)))
+    (when (and (not (file-exists-p dir))
+               (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
+      (make-directory dir t))))
