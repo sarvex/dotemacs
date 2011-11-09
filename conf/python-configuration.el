581b6c562c14f2bbd2a2009c72936653c5454f1e
Victor Deryagin <vderyagin@gmail.com>
2011-11-09 10:05:51 +0200

    python-mode: indent on newline

--- conf/python-configuration.el
+++ conf/python-configuration.el
@@ -1,2 +1,5 @@
 (dolist (x '("python" "python2" "python3" "jython"))
   (add-to-list 'interpreter-mode-alist `(,x . python-mode)))
+
+(eval-after-load 'python
+  '(define-key python-mode-map (kbd "<return>") 'reindent-then-newline-and-indent))
