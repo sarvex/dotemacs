afd059676afb1ee7d9bb4f8a7a098a094979ce2b
Victor Deryagin <vderyagin@gmail.com>
2011-12-12 11:38:51 +0200

    do not inhibit backups

--- conf/etc-configuration.el
+++ conf/etc-configuration.el
@@ -88,6 +88,7 @@
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup/"))
       tramp-backup-directory-alist '(("." . "/home/vderyagin/.emacs.d/backup/tramp/"))
+      backup-inhibited nil
       backup-by-copying t
       auto-save-list-file-name "~/.emacs.d/saves"
       auto-save-default nil
