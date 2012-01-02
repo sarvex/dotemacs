b98b8bbc2a75dd198310eaf40ec943859e63c6c9
Victor Deryagin <vderyagin@gmail.com>
2012-01-02 11:24:00 +0200

    don't need to load theme on startup, now it's done on every frame creation

--- conf/before-init-configuration.el
+++ conf/before-init-configuration.el
@@ -1,12 +1,4 @@
 (setq save-abbrevs 'silently)
 
-;; custom theme
-
-;; (when (fboundp 'customize-themes)
-;;   ;; (setq custom-theme-directory "~/.emacs.d/themes")
-;;   ;; (load-theme 'quiet-light)
-;;   ;; (enable-theme 'quiet-light)
-;;   (load-file "~/.emacs.d/themes/quiet-light-theme.el"))
-
 (load "dotemacs/misc/temporary-stuff")
 (load "dotemacs/misc/passwords.el.gpg") ; load passwords
