3ec201bd471d147b17f79eb7f6c71c6b056705fd
Victor Deryagin <vderyagin@gmail.com>
2012-01-28 09:39:44 +0200

    removed cruft, set js identation level to 2

--- conf/html-css-configuration.el
+++ conf/html-css-configuration.el
@@ -2,22 +2,9 @@
 
 (setq scss-compile-at-save nil)
 
-(setq js2-cleanup-whitespace t)
+(setq js-indent-level 2)
 
 (setq coffee-js-mode 'js-mode)
-
-(defun pretty-js-functions ()
-  (font-lock-add-keywords
-   nil `(("\\(function *\\) "
-          (0 (progn (compose-region (match-beginning 1)
-                                    (match-end 1) "ƒ")
-                    nil))))))
-
-;; (eval-after-load 'css-mode
-;;   '(add-hook 'css-mode-hook (lambda () (rainbow-mode t))))
-
-;; (eval-after-load 'js
-;;   '(add-hook 'js-mode-hook 'pretty-js-functions))
 
 (eval-after-load 'haml-mode
   '(define-key haml-mode-map (kbd "<return>") 'newline-and-indent))
