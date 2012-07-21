efcd0c750c07e9538c3f4ddac79038682601b57a
Victor Deryagin <vderyagin@gmail.com>
2012-07-21 12:57:17 +0300

    filter buffers with vc-annotate-mode

--- conf/ibuffer-configuration.el
+++ conf/ibuffer-configuration.el
@@ -238,6 +238,7 @@
             (mode . speedbar-mode)
             (mode . tags-table-mode)
             (mode . vc-dir-mode)
+            (mode . vc-annotate-mode)
             (predicate . (equal mode-name "Anything"))
 
             (name . "^\\*About GNU Emacs\\*$")
