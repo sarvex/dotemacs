5ecbcdd2e729133e403b22f032c9e4a72cb9f49d
Victor Deryagin <vderyagin@gmail.com>
2012-08-08 10:45:35 +0300

    adding more key bindings for managing projects

--- conf/projects-configuration.el
+++ conf/projects-configuration.el
@@ -1,1 +1,6 @@
-(define-key global-map (kbd "C-c p f") 'projector/find-file)
+(define-key global-map (kbd "C-c f") 'projectur-find-file)
+(define-key global-map (kbd "C-c p f") 'projectur-find-file)
+
+(define-key global-map (kbd "C-c p r") 'projectur-goto-root)
+(define-key global-map (kbd "C-c p g") 'projectur-grep)
+(define-key global-map (kbd "C-c p a") 'projectur-ack)
