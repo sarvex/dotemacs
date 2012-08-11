0b1d11f624ed5e4e7cca73b929bf10a4c402bbc4
Victor Deryagin <vderyagin@gmail.com>
2012-08-11 09:19:21 +0300

    some speedbar customizations

--- conf/cedet-configuration.el
+++ conf/cedet-configuration.el
@@ -18,14 +18,40 @@
 
 ;; Speedbar
 
-(setq speedbar-use-images t
+(setq speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|_darcs\\|\\.\\(git\\|hg\\|svn\\)\\)\\'"
+      speedbar-hide-button-brackets-flag nil
+      speedbar-indentation-width 2
+      speedbar-show-unknown-files t
+      speedbar-directory-button-trim-method 'trim
+      speedbar-tag-hierarchy-method nil
+      speedbar-tag-split-minimum-length 50
       speedbar-track-mouse-flag nil
       speedbar-update-flag nil
-      speedbar-show-unknown-files t
-      speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|_darcs\\|\\.\\(git\\|hg\\|svn\\)\\)\\'")
+      speedbar-use-images t
+      speedbar-update-flag t)
+
+
+(setq speedbar-visiting-tag-hook
+      '(speedbar-highlight-one-tag-line
+        speedbar-recenter
+        (lambda ()
+          (and
+           (derived-mode-p 'org-mode)
+           (org-show-context 'org-goto)))
+        speedbar-highlight-one-tag-line))
+
+(setq speedbar-frame-parameters
+      '((minibuffer . t)
+        (width . 20)
+        (border-width . 0)
+        (menu-bar-lines . 0)
+        (tool-bar-lines . 0)
+        (unsplittable . t)
+        (left-fringe . 0)))
 
 (eval-after-load 'speedbar
   '(progn
+    (add-to-list 'speedbar-supported-extension-expressions ".rb")
     ;; some dired-like bindings
     (define-key speedbar-key-map (kbd ";") 'speedbar-up-directory)
     (define-key speedbar-key-map (kbd "f") 'speedbar-edit-line)))
