fae5d4283fc8ca0d9ac3f469412c7b4eaa631b44
Victor Deryagin <vderyagin@gmail.com>
2012-09-06 12:24:23 +0300

    wrapped cursor modification in a function

--- conf/cursor-configuration.el
+++ conf/cursor-configuration.el
@@ -1,26 +1,27 @@
-;; Change cursor color according to mode; inspired by
+;; -*- eval: (rainbow-mode 1) -*-
+
 ;; http://www.emacswiki.org/emacs/ChangingCursorDynamically
 
-;; valid values are t, nil, box, hollow, bar, (bar . WIDTH), hbar,
-;; (hbar. HEIGHT); see the docs for set-cursor-type
-(setq my-read-only-color       "#333333"
-      my-read-only-cursor-type 'bar
-      my-overwrite-color       "#BD4747"
-      my-overwrite-cursor-type 'box
-      my-normal-color          "#333333"
-      my-normal-cursor-type    'box)
+;; valid values for `cursor-type' are t, nil, box, hollow, bar, (bar . WIDTH),
+;; hbar, (hbar. HEIGHT); see the docs for set-cursor-type
 
-(defun my-set-cursor-according-to-mode ()
-  "change cursor color and type according to some minor modes."
-  (cond
-    (buffer-read-only
-     (set-cursor-color my-read-only-color)
-     (setq cursor-type my-read-only-cursor-type))
-    (overwrite-mode
-     (set-cursor-color my-overwrite-color)
-     (setq cursor-type my-overwrite-cursor-type))
-    (t
-     (set-cursor-color my-normal-color)
-     (setq cursor-type my-normal-cursor-type))))
+(defun change-cursor-appropriately-to-circumstances ()
+  "Change cursor color and type depending on circumstances."
+  (let ((normal-color    "#333333")
+        (overwrite-color "#BD4747")
+        (read-only-color "#333333")
+        (normal-cursor-type    'box)
+        (overwrite-cursor-type 'box)
+        (read-only-cursor-type 'bar))
+    (cond
+      (buffer-read-only
+       (set-cursor-color read-only-color)
+       (setq cursor-type read-only-cursor-type))
+      (overwrite-mode
+       (set-cursor-color overwrite-color)
+       (setq cursor-type overwrite-cursor-type))
+      (t
+       (set-cursor-color normal-color)
+       (setq cursor-type normal-cursor-type)))))
 
-(add-hook 'post-command-hook 'my-set-cursor-according-to-mode)
+(add-hook 'post-command-hook 'change-cursor-appropriately-to-circumstances)
