5a180bdb7de94660d44856d3982b7cde3b29d595
Victor Deryagin <vderyagin@gmail.com>
2012-09-20 19:15:24 +0300

    simpler file associations for ruby-mode

--- conf/ruby-configuration.el
+++ conf/ruby-configuration.el
@@ -1,8 +1,5 @@
-(mapc
- (lambda (file)
-   (add-to-list 'auto-mode-alist `(,file . ruby-mode)))
- '("\\.\\(rb\\|ru\\|builder\\|rake\\|thor\\|gemspec\\)\\'"
-   "\\(rake\\|thor\\|guard\\|gem\\|cap\\|vagrant\\)file\\'"))
+(add-to-list 'auto-mode-alist '("\\.\\(rb\\|ru\\|builder\\|rake\\|thor\\|gemspec\\)\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\(rake\\|thor\\|guard\\|gem\\|cap\\|vagrant\\)file\\'" . ruby-mode))
 
 (setq ruby-electric-matching-delimeter-alist nil
       ruby-electric-expand-delimiters-list '(124))
