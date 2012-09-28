ee179586eb19f26dbf14dd14a6f3b64c363bf409
Victor Deryagin <vderyagin@gmail.com>
2012-09-28 09:28:20 +0300

    Reverting "enable `hl-line-mode' in gnus-summary" Revert "enable `hl-line-mode' in gnus-summary"
    This reverts commit f8ef29d5e7e5c18116790613e01d00b9c6d5ff1d.

--- conf/hook-configuration.el
+++ conf/hook-configuration.el
@@ -20,7 +20,6 @@
                                bookmark-bmenu-mode-hook
                                gnus-article-mode-hook
                                gnus-group-mode-hook
-                               gnus-summary-mode-hook
                                gist-list-mode-hook
                                compilation-mode-hook
                                ibuffer-mode-hook
