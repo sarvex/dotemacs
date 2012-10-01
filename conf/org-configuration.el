5072f12b09a21cab071824e68397ebced0c193fd
Victor Deryagin <vderyagin@gmail.com>
2012-10-01 08:49:22 +0300

    org: renamed keyword PAUSED -> WAITING

--- conf/org-configuration.el
+++ conf/org-configuration.el
@@ -29,13 +29,13 @@
 
 (setq
  org-todo-keywords
- '((sequence "TODO(t)" "NEXT(n)" "HOLD(h)" "STARTED(s)" "PAUSED(p)" "|" "DONE(d)" "CANCELLED(c)"))
+ '((sequence "TODO(t)" "NEXT(n)" "HOLD(h)" "STARTED(s)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)"))
  org-todo-keyword-faces
  '(("TODO" .      (:foreground "#df3800" :weight bold))
    ("NEXT" .      (:foreground "#566ea2" :weight bold))
    ("HOLD" .      (:foreground "#b68800" :weight bold))
    ("STARTED" .   (:foreground "#4d9694" :weight bold))
-   ("PAUSED" .    (:foreground "#888888" :weight bold))
+   ("WAITING" .   (:foreground "#888888" :weight bold))
    ("CANCELLED" . (:foreground "#e00051" :weight bold))
    ("DONE" .      (:foreground "#448c27" :weight bold))))
 
@@ -143,8 +143,8 @@
       org-clock-in-resume t
       org-clock-in-switch-to-state "STARTED"
       org-clock-out-remove-zero-time-clocks t
-      org-clock-out-switch-to-state "PAUSED"
-      org-clock-out-when-done '("HOLD" "PAUSED" "CANCELLED" "DONE")
+      org-clock-out-switch-to-state "WAITING"
+      org-clock-out-when-done '("HOLD" "WAITING" "CANCELLED" "DONE")
       org-clock-persist 'history
       org-clock-report-include-clocking-task t)
 
