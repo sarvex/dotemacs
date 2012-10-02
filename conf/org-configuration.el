eed2059725b204d758f1ab32fc5d138547b43078
Victor Deryagin <vderyagin@gmail.com>
2012-10-02 18:57:34 +0300

    removed binding for 'r' in agenda

--- conf/org-configuration.el
+++ conf/org-configuration.el
@@ -214,14 +214,7 @@
      (define-key org-agenda-mode-map (kbd "C-S-<right>") nil)
 
      (define-key org-agenda-mode-map (kbd "M-p") 'org-previous-link)
-     (define-key org-agenda-mode-map (kbd "M-n") 'org-next-link)
-
-     (define-key org-agenda-mode-map (kbd "r")
-       (lambda ()
-         "Rebuild all agenda views in current buffer and jump to today."
-         (interactive)
-         (org-agenda-redo 'rebuild-all)
-         (org-agenda-goto-today))))))
+     (define-key org-agenda-mode-map (kbd "M-n") 'org-next-link))))
 
 
 (defun vderyagin/find-org-file ()
