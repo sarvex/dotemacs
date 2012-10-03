266f2390d188f40db7be741112262fad2a5a2c9d
Victor Deryagin <vderyagin@gmail.com>
2012-10-03 09:27:17 +0300

    cleaned up ruby configuration

--- conf/ruby-configuration.el
+++ conf/ruby-configuration.el
@@ -38,34 +38,20 @@
     (require 'rcodetools)
 
     (inf-ruby-keys)
-    (define-key inferior-ruby-mode-map (kbd "<f1>") 'yari)
     (define-key inferior-ruby-mode-map (kbd "C-l") 'recenter-top)
 
-    ;; enable flymake
-    (require 'flymake)
-
-    (mapc
-     (lambda (file)
-       (push `(,file flymake-ruby-init) flymake-allowed-file-name-masks))
-     '(".+\\.\\(rb\\|ru\\|rake\\|thor\\|gemspec\\)\\'"
-       "\\([Rr]ake\\|[Tt]hor\\|[Gg]\\(uard\\|em\\)\\|[Cc]ap\\|[Vv]agrant\\)file\\'"))
-
     (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+    (push
+     '("\\.\\(rb\\|ru\\|builder\\|rake\\|thor\\|gemspec\\)\\'" flymake-ruby-init)
+     flymake-allowed-file-name-masks)
 
     (define-key ruby-mode-map (kbd "C-c C-c") 'projectur-rspec)
+    (define-key ruby-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
     (define-key ruby-mode-map (kbd "<f9>")
      (lambda () (interactive) (call-interactively 'xmp) (delete-trailing-whitespace)))
-    (define-key ruby-mode-map (kbd "<f1>") 'yari)
-    (define-key ruby-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
-    (define-key ruby-mode-map (kbd "C-<return>")
-     (lambda () (interactive) (move-end-of-line 1) (reindent-then-newline-and-indent)))
-    (define-key ruby-mode-map (kbd "M-<return>")
-     (lambda () (interactive) (previous-line 1) (move-end-of-line 1) (reindent-then-newline-and-indent)))
 
     (add-hook 'ruby-mode-hook 'my-ruby-mode-hook)))
 
-(eval-after-load 'yari
-  '(define-key yari-mode-map (kbd "<f1>") 'yari))
 
 (defadvice ruby-indent-exp (after delete-trailing-whitespace-on-indention activate)
   "Clean up buffer of trailing whitespaces after indentation."
