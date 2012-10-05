a388300135fa0845431f63921439ee6fbd40b23f
Victor Deryagin <vderyagin@gmail.com>
2012-10-05 11:02:09 +0300

    yasnippet: explicitly required dropdown-list

--- conf/yasnippet-configuration.el
+++ conf/yasnippet-configuration.el
@@ -14,16 +14,16 @@
 
 (eval-after-load 'yasnippet
   '(progn
+    (require 'dropdown-list)
+
     (setq yas-snippet-dirs '("~/.emacs.d/yasnippets"))
 
-    (add-hook 'snippet-mode-hook 'dont-require-final-newline)
+    (add-hook 'snippet-mode-hook (lambda () (setq require-final-newline nil)))
     (add-hook 'snippet-mode-hook
      (lambda ()
        (add-hook 'after-save-hook 'yas-recompile-all 'append 'make-it-local)
        (add-hook 'after-save-hook 'yas-reload-all 'append 'make-it-local)))))
 
-(defun dont-require-final-newline ()
-  (setq require-final-newline nil))
 
 (add-to-list 'auto-mode-alist '("\\.yasnippet\\'" . snippet-mode))
 
