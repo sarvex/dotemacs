1529f7a90984e41b7e374341465eabd785713b3c
Victor Deryagin <vderyagin@gmail.com>
2012-10-10 14:21:36 +0300

    using `custom-set-variables' for helm configuration

--- conf/helm-configuration.el
+++ conf/helm-configuration.el
@@ -1,14 +1,14 @@
-(setq helm-buffer-max-length 40
-      helm-c-default-external-file-browser "pcmanfm"
-      helm-c-kill-ring-max-lines-number 5
-      helm-candidate-number-limit 1000
-      helm-command-prefix-key "C-c h"
-      helm-google-suggest-use-curl-p t)
-
-(define-key global-map (kbd "C-c h") helm-command-map)
-
 (require 'helm-config)
 
+(custom-set-variables
+ '(helm-buffer-max-length 40)
+ '(helm-c-default-external-file-browser "pcmanfm")
+ '(helm-c-kill-ring-max-lines-number 5)
+ '(helm-candidate-number-limit 1000)
+ '(helm-command-prefix-key "C-c h")
+ '(helm-google-suggest-use-curl-p t))
+
+(define-key global-map (kbd "C-c h") helm-command-map)
 (define-key global-map (kbd "C-c h o") 'helm-occur)
 (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
 (define-key global-map (kbd "C-x f") 'helm-recentf)
