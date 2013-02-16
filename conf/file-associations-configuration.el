bb42bf61f7018d00d9386b4a48cb52cb0ffdeef8
Victor Deryagin <vderyagin@gmail.com>
2013-02-16 06:30:28 +0200

    file associations for mplayer

--- conf/file-associations-configuration.el
+++ conf/file-associations-configuration.el
@@ -5,7 +5,7 @@
      ("\\.\\(bmp\\|ico\\)\\'" "feh" (file))
      ("\\.cb[abgrz]\\'" "qcomicbook" (file))
      ("\\.chm\\'" "kchmviewer" (file))
-     ("\\.pdf\\'" "mupdf" (file))
+     ("\\.pdf\\'"  "mupdf -r 96" (file))
      (,(concat "\\."
                "\\("
                "djvu"
@@ -91,14 +91,14 @@
                "\\'")
       "libreoffice" (file))))
  '(dired-guess-shell-alist-user
-   '(("\\.pdf\\'" '("mupdf" "qpdfview --unique"))
+   '(("\\.pdf\\'" '("mupdf -r 96" "qpdfview --unique"))
      ("\\.\\(djvu\\|ps\\|dvi\\)\\'" "evince")
      ("\\.cb[abgrz]\\'" "qcomicbook")
-
      ("\\.tar\\(\\.Z\\)?\\'" "aunpack")
      ("\\.t\\(ar\\.\\)?gz\\'" "aunpack")
      ("\\.t\\(ar\\.bz2\\|bz\\)\\'" "aunpack")
      ("\\.t\\(ar\\.\\)?xz\\'" "aunpack")
      ("\\.\\(zip\\|rar\\|7z\\|xz\\|bz2\\|gz\\|arc\\)\\'" "aunpack" )
      ("\\.\\(bmp\\|ico\\|jpe?g\\|gif\\|tiff?\\|png\\)\\'" "feh")
+     ("\\.\\(mpe?g\\|avi\\|m[ko]v\\|vob\\|ogv\\|webm\\|m[24]v\\|flv\\|mp[24]\\|asf\\|rm\\(vb\\)?\\|wmv\\|divx\\|ts\\)\\(\\.\\(part\\|crdownload\\)\\)?\\'" '("mplayer" "mplayer -ass-bottom-margin 100 2>/dev/null >&2"))
      ("\\.\\(mp3\\|ogg\\|flac\\|wav\\|m[k4]a\\|wma\\|ac3\\|aac\\|ape\\)\\'" "vlc"))))
