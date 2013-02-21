2fa473501cddfa233e48fe9138a4b40e03575d0a
Victor Deryagin <vderyagin@gmail.com>
2013-02-21 11:44:44 +0200

    haskell hooks

--- conf/haskell-configuration.el
+++ conf/haskell-configuration.el
@@ -1,10 +1,4 @@
-;; (setq haskell-font-lock-symbols t)
-
-(defun pretty-lambdas-haskell ()
-  (font-lock-add-keywords
-   nil `((,(concat "(?\\(" (regexp-quote "\\") "\\)")
-           (0 (progn (compose-region (match-beginning 1) (match-end 1)
-                                     ,(make-char 'greek-iso8859-7 107))
-                     nil))))))
-
-;; (add-hook 'haskell-mode-hook 'pretty-lambdas-haskell)
+(eval-after-load 'haskell-mode
+  '(progn
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)))
