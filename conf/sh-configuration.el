a124c1f756b2b0ea982885f4c23f9503b9c4f173
Victor Deryagin <vderyagin@gmail.com>
2013-03-02 20:02:49 +0200

    run shell script with f9

--- conf/sh-configuration.el
+++ conf/sh-configuration.el
@@ -2,4 +2,6 @@
  '(sh-basic-offset 2))
 
 (eval-after-load 'sh-script
-  '(define-key sh-mode-map (kbd "<return>") 'reindent-then-newline-and-indent))
+  '(progn
+    (define-key sh-mode-map (kbd "<f9>") 'executable-interpret)
+    (define-key sh-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)))
