8897faf191ce0fb97366d5a5dedb66e406bed110
Victor Deryagin <vderyagin@gmail.com>
2013-03-13 08:57:09 +0200

    flattened some hooks setup

--- conf/hook-configuration.el
+++ conf/hook-configuration.el
@@ -1,135 +1,126 @@
-(let* ((conf-modes '(conf-colon-mode
-                     conf-javaprop-mode
-                     conf-mode
-                     conf-ppd-mode
-                     conf-space-mode
-                     conf-unix-mode
-                     conf-windows-mode
-                     conf-xdefaults-mode))
-       (conf-modes-hooks (mapcar (lambda (mode)
-                                   (intern (concat (symbol-name mode)
-                                                   "-hook")))
-                                 conf-modes))
-       (hl-line-modes-hooks (append
-                             conf-modes-hooks
-                             '(Info-mode-hook
-                               archive-mode-hook
-                               bookmark-bmenu-mode-hook
-                               compilation-mode-hook
-                               dired-mode-hook
-                               emacs-lisp-mode-hook
-                               gist-list-mode-hook
-                               gnus-article-mode-hook
-                               gnus-summary-mode-hook
-                               gnus-group-mode-hook
-                               go-mode-hook
-                               ibuffer-mode-hook
-                               irfc-mode-hook
-                               org-agenda-mode-hook
-                               scheme-mode-hook
-                               special-mode-hook
-                               speedbar-mode-hook
-                               tar-mode-hook
-                               twittering-mode-hook
-                               vc-annotate-mode-hook
-                               woman-mode-hook)))
-       (whitespace-modes-hooks (append
-                                conf-modes-hooks
-                                '(text-mode-hook
-                                  change-log-mode-hook
-                                  coffee-mode-hook
-                                  css-mode-hook
-                                  feature-mode-hook
-                                  graphviz-dot-mode-hook
-                                  haml-mode-hook
-                                  haskell-mode-hook
-                                  lisp-interaction-mode-hook
-                                  makefile-mode-hook
-                                  markdown-mode-hook
-                                  nxml-mode-hook
-                                  ruby-mode-hook
-                                  scala-mode-hook
-                                  sgml-mode-hook
-                                  sh-mode-hook
-                                  yaml-mode-hook)))
-       (hl-keywords-modes-hooks (append
-                                 conf-modes-hooks
-                                 '(c-mode-common-hook
-                                   coffee-mode-hook
-                                   cperl-mode-hook
-                                   css-mode-hook
-                                   emacs-lisp-mode-hook
-                                   feature-mode-hook
-                                   go-mode-hook
-                                   graphviz-dot-mode-hook
-                                   haml-mode-hook
-                                   haskell-mode-hook
-                                   js-mode-hook
-                                   lisp-interaction-mode-hook
-                                   lisp-mode-hook
-                                   makefile-mode-hook
-                                   nxml-mode-hook
-                                   python-mode-hook
-                                   ruby-mode-hook
-                                   scala-mode-hook
-                                   sgml-mode-hook
-                                   sh-mode-hook
-                                   yaml-mode-hook)))
-       (drag-stuff-modes-hooks (append
-                                conf-modes-hooks
-                                '(c-mode-common-hook
-                                  go-mode-hook
-                                  coffee-mode-hook
-                                  cperl-mode-hook
-                                  css-mode-hook
-                                  feature-mode-hook
-                                  graphviz-dot-mode-hook
-                                  haml-mode-hook
-                                  haskell-mode-hook
-                                  js-mode-hook
-                                  magit-log-edit-mode-hook
-                                  makefile-mode-hook
-                                  nxml-mode-hook
-                                  python-mode-hook
-                                  ruby-mode-hook
-                                  scala-mode-hook
-                                  sgml-mode-hook
-                                  sh-mode-hook
-                                  yaml-mode-hook)))
-       (eldoc-modes-hooks '(c-mode-hook
-                            emacs-lisp-mode-hook
-                            inferior-emacs-lisp-mode-hook
-                            cperl-mode-hook)))
+(mapc
+ (lambda (h)
+   (add-hook h 'hl-line-mode))
+ '(
+   Info-mode-hook
+   Man-mode-hook
+   archive-mode-hook
+   bookmark-bmenu-mode-hook
+   compilation-mode-hook
+   conf-mode-hook
+   dired-mode-hook
+   emacs-lisp-mode-hook
+   gist-list-mode-hook
+   gnus-article-mode-hook
+   gnus-group-mode-hook
+   gnus-summary-mode-hook
+   go-mode-hook
+   ibuffer-mode-hook
+   irfc-mode-hook
+   org-agenda-mode-hook
+   scheme-mode-hook
+   special-mode-hook
+   speedbar-mode-hook
+   tar-mode-hook
+   twittering-mode-hook
+   vc-annotate-mode-hook
+   woman-mode-hook
+   ))
 
-  ;; hl-line
-  (mapc
-   (lambda (hooks)
-     (add-hook hooks (lambda () (hl-line-mode t))))
-   hl-line-modes-hooks)
 
-  ;; whitespaces
-  (mapc
-   (lambda (hooks)
-     (add-hook hooks (lambda () (whitespace-mode t))))
-   whitespace-modes-hooks)
+(mapc
+ (lambda (h)
+   (add-hook h 'whitespace-mode))
+ '(
+   change-log-mode-hook
+   coffee-mode-hook
+   conf-mode-hook
+   css-mode-hook
+   feature-mode-hook
+   graphviz-dot-mode-hook
+   haml-mode-hook
+   haskell-mode-hook
+   lisp-interaction-mode-hook
+   makefile-mode-hook
+   markdown-mode-hook
+   nxml-mode-hook
+   ruby-mode-hook
+   scala-mode-hook
+   sgml-mode-hook
+   sh-mode-hook
+   text-mode-hook
+   yaml-mode-hook
+   ))
 
-  ;; highlight keywords
-  (mapc
-   (lambda (hooks)
-     (add-hook hooks 'turn-on-hl-keywords))
-   hl-keywords-modes-hooks)
 
-  ;; drag-stuff mode
-  (mapc
-   (lambda (hooks)
-     (add-hook hooks 'turn-on-drag-stuff-mode))
-   drag-stuff-modes-hooks)
 
-  ;; eldoc
-  (mapc
-   (lambda (hooks)
-     (add-hook hooks 'turn-on-eldoc-mode))
-   eldoc-modes-hooks))
+
+(mapc
+ (lambda (h)
+   (add-hook h 'turn-on-hl-keywords))
+ '(
+   c-mode-common-hook
+   coffee-mode-hook
+   conf-mode-hook
+   cperl-mode-hook
+   css-mode-hook
+   emacs-lisp-mode-hook
+   feature-mode-hook
+   go-mode-hook
+   graphviz-dot-mode-hook
+   haml-mode-hook
+   haskell-mode-hook
+   js-mode-hook
+   lisp-interaction-mode-hook
+   lisp-mode-hook
+   makefile-mode-hook
+   nxml-mode-hook
+   python-mode-hook
+   ruby-mode-hook
+   scala-mode-hook
+   sgml-mode-hook
+   sh-mode-hook
+   yaml-mode-hook
+   ))
+
+
+(mapc
+ (lambda (h)
+   (add-hook h 'turn-on-drag-stuff-mode))
+ '(
+   c-mode-common-hook
+   coffee-mode-hook
+   conf-mode-hook
+   cperl-mode-hook
+   css-mode-hook
+   feature-mode-hook
+   go-mode-hook
+   graphviz-dot-mode-hook
+   haml-mode-hook
+   haskell-mode-hook
+   js-mode-hook
+   magit-log-edit-mode-hook
+   makefile-mode-hook
+   nxml-mode-hook
+   python-mode-hook
+   ruby-mode-hook
+   scala-mode-hook
+   sgml-mode-hook
+   sh-mode-hook
+   yaml-mode-hook
+   ))
+
+
+(mapc
+ (lambda (h)
+   (add-hook h 'turn-on-eldoc-mode))
+ '(
+   c-mode-hook
+   cperl-mode-hook
+   emacs-lisp-mode-hook
+   inferior-emacs-lisp-mode-hook
+   ))
+
 
 (defun turn-on-hl-keywords ()
   (font-lock-add-keywords
