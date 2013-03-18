9844c90b0f5888f5c18ab5505acf3a480f3fcf89
Victor Deryagin <vderyagin@gmail.com>
2013-03-18 10:20:18 +0200

    helm: key binding for google suggest

--- conf/helm-configuration.el
+++ conf/helm-configuration.el
@@ -14,6 +14,8 @@
 (define-key global-map (kbd "C-c h") helm-command-map)
 
 (define-key global-map (kbd "C-c h t") 'helm-world-time)
+(define-key global-map (kbd "C-c h g") 'helm-google-suggest)
 (define-key global-map (kbd "C-c h o") 'helm-occur)
+
 (define-key global-map (kbd "C-x f") 'helm-recentf)
 (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
