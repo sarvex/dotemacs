048efa5c8b735a4de577aa83fb96ee2954ef9548
Victor Deryagin <vderyagin@gmail.com>
2013-03-23 19:02:08 +0200

    cleaned up slime configuration

--- conf/slime-configuration.el
+++ conf/slime-configuration.el
@@ -1,20 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 
-(setq common-lisp-hyperspec-root "file:/usr/share/doc/hyperspec/HyperSpec/"
-      inferior-lisp-program "/usr/bin/sbcl --noinform"
-      slime-autodoc-use-multiline-p t
-      slime-header-line-p nil
-      slime-net-coding-system 'utf-8-unix
-      slime-repl-history-file "~/.emacs.d/slime-history.eld"
-      slime-repl-history-remove-duplicates t
-      slime-repl-history-size 2000
-      slime-repl-history-trim-whitespaces t
-      slime-startup-animation nil
-      slime-truncate-lines nil)
-
-(setq slime-lisp-implementations
-      '((sbcl ("/usr/bin/sbcl" "--noinform") :coding-system utf-8-unix)))
-
+(setq inferior-lisp-program "/usr/bin/sbcl --noinform"
+      common-lisp-hyperspec-root "file:/usr/share/doc/hyperspec/HyperSpec/")
 
 (require 'slime-autoloads)
 
@@ -22,10 +9,19 @@
   '(progn
     (slime-setup '(slime-fancy))
 
-    (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
     (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
 
     ;; Stop SLIME's REPL from grabbing DEL, which is annoying when backspacing over a '('
     (define-key slime-repl-mode-map (read-kbd-macro paredit-backward-delete-key) nil)
 
     (define-key slime-repl-mode-map (kbd "C-l") 'recenter-top)))
+
+(custom-set-variables
+ '(slime-autodoc-use-multiline-p t)
+ '(slime-header-line-p nil)
+ '(slime-net-coding-system 'utf-8-unix)
+ '(slime-repl-history-file "~/.emacs.d/slime-history.eld")
+ '(slime-repl-history-remove-duplicates t)
+ '(slime-repl-history-size 2000)
+ '(slime-repl-history-trim-whitespaces t)
+ '(slime-startup-animation nil))
