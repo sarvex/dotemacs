6dced67d841cf54d7de530bc61d1a0ee87f09713
Victor Deryagin <vderyagin@gmail.com>
2013-03-24 21:31:56 +0200

    simpler erc setup

--- conf/erc-configuration.el
+++ conf/erc-configuration.el
@@ -1,9 +1,23 @@
 ;;; -*- lexical-binding: t -*-
 
-(eval-when-compile
-  (defvar my-freenode-username)
-  (defvar my-freenode-password)
-  (defvar my-full-name))
+(defun vderyagin/switch-to-irc-buffer ()
+  "Connect to IRC server or switch to an IRC buffer if connected already."
+  (interactive)
+  (let ((buffers (and (fboundp 'erc-buffer-list)
+                      (erc-buffer-list))))
+    (if buffers
+        (switch-to-buffer (ido-completing-read
+                           "IRC channel: "
+                           (mapcar 'buffer-name buffers)
+                           nil
+                           'require-match))
+        (when (y-or-n-p "Connect to IRC? ") (erc)))))
+
+(eval-after-load 'erc
+  '(progn
+    ;; (erc-update-modules)
+    (define-key erc-mode-map (kbd "<return>") 'browse-url-at-point)
+    (define-key erc-mode-map (kbd "C-c C-<return>") 'erc-send-current-line)))
 
 (custom-set-variables
  '(erc-fill-column 67)
@@ -18,13 +32,11 @@
  '(erc-autojoin-timing 'ident)
  '(erc-autojoin-delay most-positive-fixnum)
 
- '(erc-track-exclude-server-buffer t)
- '(erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "324" "329" "332" "333" "353" "477"))
-
  '(erc-kill-buffer-on-part t)
  '(erc-kill-server-buffer-on-quit t)
  '(erc-kill-queries-on-quit t)
 
+ '(erc-anonymous-login nil)
  '(erc-header-line-format nil)
  '(erc-hide-list '("324" "329" "333" "353" "MODE" "JOIN" "PART" "QUIT" ))
  '(erc-hide-prompt nil)
@@ -38,63 +50,26 @@
  '(erc-truncate-buffer-on-save t)
  '(erc-warn-about-blank-lines t)
 
- '(erc-modules '(autojoin
+ '(erc-modules '(
+                 autojoin
                  button
                  completion
                  fill
+                 highlight-nicknames
                  irccontrols
                  list
                  log
                  match
-                 menu
                  move-to-prompt
                  netsplit
                  networks
                  noncommands
                  readonly
                  ring
-                 highlight-nicknames
                  scrolltobottom
+                 services
                  smiley
+                 spelling
                  stamp
-                 truncate)))
-
-(eval-after-load 'erc
-  '(progn
-    (require 'erc-highlight-nicknames)
-    (require 'erc-services)
-    (require 'erc-join)
-    (require 'erc-fill)
-
-    (erc-update-modules)
-
-    ;; (require 'erc-track)
-    ;; (erc-track-mode t)
-
-    (define-key erc-mode-map (kbd "<return>") 'browse-url-at-point)
-    (define-key erc-mode-map (kbd "C-c C-<return>") 'erc-send-current-line)))
-
-(defun vderyagin/erc-join ()
-  (interactive)
-  (when (y-or-n-p "Connect to IRC? ")
-    (erc-services-mode)
-    (erc-autojoin-mode)
-
-    (erc-select :server "irc.freenode.net"
-                :port 6667
-                :nick my-freenode-username
-                :password my-freenode-password
-                :full-name my-full-name)))
-
-(defun vderyagin/switch-to-irc-buffer ()
-  "Switch to an IRC buffer, or run `vderyagin/erc-join'."
-  (interactive)
-  (let ((buffers (and (fboundp 'erc-buffer-list)
-                      (erc-buffer-list))))
-    (if buffers
-        (switch-to-buffer (ido-completing-read
-                           "IRC channel: "
-                           (mapcar 'buffer-name buffers)
-                           nil
-                           'require-match))
-        (call-interactively 'vderyagin/erc-join))))
+                 truncate
+                 )))
