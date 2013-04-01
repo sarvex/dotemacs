d83ef1bb552ba9d43137b9c84e9a697e51c43c49
Victor Deryagin <vderyagin@gmail.com>
2013-04-01 08:57:46 +0300

    enabled magit-wip

--- conf/git-configuration.el
+++ conf/git-configuration.el
@@ -1,5 +1,20 @@
 ;;; -*- lexical-binding: t -*-
 
+(magit-wip-mode 1)
+(global-magit-wip-save-mode)
+
+(add-hook 'magit-log-edit-mode-hook 'turn-on-auto-fill)
+(add-hook 'magit-log-edit-mode-hook 'flyspell-mode)
+
+;; solving conflicts with my windows-operating keys
+(define-key magit-mode-map (kbd "M-1") nil)
+(define-key magit-mode-map (kbd "M-2") nil)
+(define-key magit-mode-map (kbd "M-3") nil)
+(define-key magit-mode-map (kbd "M-4") nil)
+
+(define-key magit-mode-map (kbd "M-n") 'magit-show-commit-forward)
+(define-key magit-mode-map (kbd "M-p") 'magit-show-commit-backward)
+
 (custom-set-variables
  '(magit-commit-all-when-nothing-staged 'ask-stage)
  '(magit-log-auto-more t)
@@ -8,17 +23,3 @@
  '(magit-repo-dirs-depth 6)
  '(magit-save-some-buffers t)
  '(magit-status-buffer-switch-function 'switch-to-buffer))
-
-(eval-after-load 'magit
-  '(progn
-    ;; solving conflicts with my windows-operating keys
-    (define-key magit-mode-map (kbd "M-1") nil)
-    (define-key magit-mode-map (kbd "M-2") nil)
-    (define-key magit-mode-map (kbd "M-3") nil)
-    (define-key magit-mode-map (kbd "M-4") nil)
-
-    (define-key magit-mode-map (kbd "M-n") 'magit-show-commit-forward)
-    (define-key magit-mode-map (kbd "M-p") 'magit-show-commit-backward)
-
-    (add-hook 'magit-log-edit-mode-hook 'turn-on-auto-fill)
-    (add-hook 'magit-log-edit-mode-hook 'flyspell-mode)))
