0c82471a0dfc7e76b77811300736c6fb3fd070dc
Victor Deryagin <vderyagin@gmail.com>
2013-04-04 13:09:35 +0300

    ruby: using flycheck instead of flymake

--- conf/ruby-configuration.el
+++ conf/ruby-configuration.el
@@ -7,47 +7,21 @@
 (custom-set-variables
  '(ruby-electric-expand-delimiters-list '(124)))
 
-(defun flymake-ruby-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-intemp))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "ruby" (list "-c" local-file))))
-
-(defun flymake-ruby-enable ()
-  (when (and buffer-file-name
-             (file-writable-p
-              (file-name-directory buffer-file-name))
-             (file-writable-p buffer-file-name)
-             (if (fboundp 'tramp-list-remote-buffers)
-                 (not (member (current-buffer) (tramp-list-remote-buffers)))
-                 t))
-    (local-set-key (kbd "C-c e")
-                   'flymake-display-err-menu-for-current-line)
-    (flymake-mode t)))
-
 (eval-after-load 'ruby-mode
   (quote
    (progn
      (require 'ruby-electric)
      (require 'inf-ruby)
      (require 'rcodetools)
-     (require 'flymake)
      (require 'ffap)
 
      (inf-ruby-keys)
-
-     (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)
-           flymake-err-line-patterns)
-     (push (list (rx not-newline "." (or "rb" "ru" "builder" "rake" "thor" "gemspec") string-end) 'flymake-ruby-init)
-           flymake-allowed-file-name-masks)
 
      (define-key ruby-mode-map (kbd "C-c C-c") 'projectur-rspec)
      (define-key ruby-mode-map (kbd "<f9>") 'xmp)
      (define-key ruby-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
 
-     (add-hook 'ruby-mode-hook 'flymake-ruby-enable)
+     (add-hook 'ruby-mode-hook 'flycheck-mode)
      (add-hook 'ruby-mode-hook (lambda () (ruby-electric-mode t)))
      (add-hook 'ruby-mode-hook (lambda () (setq comment-column 42)))
 
