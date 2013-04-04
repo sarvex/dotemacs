52f6219e5ec7021fb8e82bb48d3c1f39b5549d75
Victor Deryagin <vderyagin@gmail.com>
2013-04-04 11:12:37 +0300

    applied back some changes

--- conf/keys-configuration.el
+++ conf/keys-configuration.el
@@ -45,10 +45,8 @@
 (define-key prog-mode-map (kbd "<return>") 'newline-and-indent)
 
 ;; window switching
-(define-key global-map (kbd "C-<tab>")
-  (lambda () (interactive) (select-window (next-window))))
-(define-key global-map (kbd "C-S-<iso-lefttab>")
-  (lambda () (interactive) (select-window (previous-window))))
+(define-key global-map (kbd "C-<tab>") 'other-window)
+(define-key global-map (kbd "C-S-<iso-lefttab>") (lambda () (interactive) (other-window -1)))
 
 ;; window resizing
 (define-key global-map (kbd "S-C-<left>") 'shrink-window-horizontally)
@@ -59,8 +57,7 @@
 ;; text scale
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
-(define-key global-map (kbd "C-=")
-  (lambda () (interactive) (text-scale-set 0)))
+(define-key global-map (kbd "C-=") (lambda () (interactive) (text-scale-set 0)))
 
 ;; Good bye, mouse buttons and arrow keys
 (mapc
