7eea8c3e9c1fcece1d2d816b360a11474b3f77e9
Victor Deryagin <vderyagin@gmail.com>
2013-04-04 13:15:01 +0300

    go: using flycheck instead of flymake

--- conf/go-configuration.el
+++ conf/go-configuration.el
@@ -1,30 +1,17 @@
 ;;; -*- lexical-binding: t -*-
 
 (defun vderyagin/go-mode-hook ()
-  (require 'flymake)
-  (add-to-list 'flymake-allowed-file-name-masks '(".+\\.go\\'" flymake-go-init))
-  (flymake-mode -1)
-
   (add-hook 'before-save-hook 'gofmt nil 'make-it-local)
-
-  (require 'go-autocomplete)
-
   (set (make-local-variable 'compile-command) "go test")
-
   (setq tab-width 2
         show-trailing-whitespace t))
 
-(defun flymake-go-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "go" (list "build" "-o" "/dev/null" temp-file))))
-
 (eval-after-load 'go-mode
   '(progn
+    (require 'go-autocomplete)
+
     (add-hook 'go-mode-hook 'vderyagin/go-mode-hook)
+    (add-hook 'go-mode-hook 'flycheck-mode)
 
     (define-key go-mode-map (kbd "M-i") 'go-import-add)
     (define-key go-mode-map (kbd "C-c C-r") 'go-remove-unused-imports)
@@ -33,8 +20,6 @@
     (define-key go-mode-map (kbd "C-?") 'godef-describe)
 
     (define-key go-mode-map (kbd "<f9>") 'compile)
-    (define-key go-mode-map (kbd "<f10>") 'flymake-mode)
 
     (define-key go-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
-    (define-key go-mode-map (kbd "C-c e") 'flymake-display-err-menu-for-current-line)
     (define-key go-mode-map (kbd "C-c C-c") 'gofmt)))
