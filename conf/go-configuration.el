c6b1ad0c0bd7c35856fe7b070f6643480c9307aa
Victor Deryagin <vderyagin@gmail.com>
2013-04-04 14:11:14 +0300

    changed named hook into lambda

--- conf/go-configuration.el
+++ conf/go-configuration.el
@@ -1,17 +1,17 @@
 ;;; -*- lexical-binding: t -*-
 
-(defun vderyagin/go-mode-hook ()
-  (add-hook 'before-save-hook 'gofmt nil 'make-it-local)
-  (set (make-local-variable 'compile-command) "go test")
-  (setq tab-width 2
-        show-trailing-whitespace t))
-
 (eval-after-load 'go-mode
   '(progn
     (require 'go-autocomplete)
 
-    (add-hook 'go-mode-hook 'vderyagin/go-mode-hook)
     (add-hook 'go-mode-hook 'flycheck-mode)
+
+    (add-hook 'go-mode-hook
+     (lambda ()
+       (add-hook 'before-save-hook 'gofmt nil 'make-it-local)
+       (set (make-local-variable 'compile-command) "go test")
+       (setq tab-width 2
+             show-trailing-whitespace t)))
 
     (define-key go-mode-map (kbd "M-i") 'go-import-add)
     (define-key go-mode-map (kbd "C-c C-r") 'go-remove-unused-imports)
