a88e102270b799322043be1d765ceed952853d8c
Victor Deryagin <vderyagin@gmail.com>
2013-04-05 20:46:14 +0300

    smarter selection of build command

--- conf/rust-configuration.el
+++ conf/rust-configuration.el
@@ -6,7 +6,13 @@
      (add-hook 'rust-mode-hook
                (lambda ()
                  (setq show-trailing-whitespace t)
-                 (set (make-local-variable 'compile-command) "rust test")))
+                 (set (make-local-variable 'compile-command)
+                      (format "rust %s %s"
+                              (if (string-match-p (rx not-newline "-test.rs" string-end)
+                                                  buffer-file-name)
+                                  "test"
+                                  "build")
+                              (file-name-nondirectory buffer-file-name)))))
 
      (define-key rust-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
      (define-key rust-mode-map (kbd "<f9>") 'compile))))
