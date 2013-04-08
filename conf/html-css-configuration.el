6ae59296720804592a3ef60f1ae404d18b214ae9
Victor Deryagin <vderyagin@gmail.com>
2013-04-08 13:05:22 +0300

    enable yasnippet in `html-mode'

--- conf/html-css-configuration.el
+++ conf/html-css-configuration.el
@@ -5,8 +5,10 @@
  '(scss-compile-at-save nil))
 
 (eval-after-load 'sgml-mode
-  '(define-key sgml-mode-map (kbd "<return>") 'reindent-then-newline-and-indent))
-
+  (quote
+   (progn
+     (add-hook 'html-mode-hook 'yas-minor-mode-on)
+     (define-key sgml-mode-map (kbd "<return>") 'reindent-then-newline-and-indent))))
 
 (mapc
  (lambda (hooks)
