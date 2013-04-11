96f2066e336d1ccb98771cbebb712049436e16d1
Victor Deryagin <vderyagin@gmail.com>
2013-04-11 20:08:05 +0300

    not using `savehist' any more

--- conf/hist-configuration.el
+++ conf/hist-configuration.el
@@ -1,31 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 
 (custom-set-variables
- '(savehist-file "~/.emacs.d/savehist")
- '(savehist-save-minibuffer-history t)
- '(savehist-additional-variables '(global-mark-ring
-                                   ido-work-directory-list
-                                   kill-ring
-                                   kmacro-ring
-                                   projectur-history
-                                   regexp-search-ring
-                                   register-alist
-                                   search-ring)))
-
-(savehist-mode)
-
-(add-hook 'savehist-save-hook
-          (lambda ()
-            "Remove duplicates in saved variables."
-            (mapc
-             (lambda (var)
-               (when (boundp var)
-                 (setq var (delete-dups (symbol-value var)))))
-             (append savehist-additional-variables
-                     savehist-minibuffer-history-variables))))
-
-
-(custom-set-variables
  '(recentf-save-file "~/.emacs.d/recentf")
  '(recentf-max-saved-items 3000)
  '(recentf-exclude
@@ -46,29 +21,33 @@
  '(desktop-path '("~/.emacs.d/"))
  '(desktop-load-locked-desktop t)
  '(desktop-save t)
- '(desktop-save-mode t)
- '(desktop-globals-to-save nil)      ; not needed, `savehist' does the thing
  '(desktop-files-not-to-save
    (rx (or
-        (regexp "^/[^/:]*:")
-        (regexp "(ftp)$")
+        (and line-start "/" (* (not (any "/:"))) ":")
+        (and "(ftp)" line-end)
         (and ".gpg" string-end)
         "/.emacs.d/recentf"
         (and "loaddefs.el" string-end)
         (and "/" (or "org-clock-save.el" "bmk-bmenu-state.el" "ido-history" "newsrc-dribble") string-end)
         (eval (expand-file-name "~/repos/"))
-        (eval (expand-file-name "~/org/")))))
- '(desktop-locals-to-save
-   '(desktop-locals-to-save
-     truncate-lines
-     case-fold-search
-     case-replace
-     fill-column
-     overwrite-mode
-     change-log-default-name
-     size-indication-mode
-     buffer-file-coding-system
-     indent-tabs-mode
-     show-trailing-whitespace)))
+        (eval (expand-file-name "~/org/"))))))
 
 (desktop-save-mode t)
+
+(mapc
+ (lambda (sym)
+   (add-to-list 'desktop-globals-to-save sym))
+ '(global-mark-ring
+   ido-work-directory-list
+   kill-ring
+   kmacro-ring
+   projectur-history))
+
+(add-hook 'desktop-save-hook
+          (lambda ()
+            "Remove duplicates in saved variables."
+            (mapc
+             (lambda (var)
+               (when (boundp var)
+                 (setq var (delete-dups (symbol-value var)))))
+             desktop-globals-to-save)))
