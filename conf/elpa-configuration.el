f4d7110bf9e6fc6c42e40fc79753f9fd93f8d412
Victor Deryagin <vderyagin@gmail.com>
2013-04-12 14:10:30 +0300

    not using "tromey.com/elpa" package archive

--- conf/elpa-configuration.el
+++ conf/elpa-configuration.el
@@ -4,6 +4,6 @@
 
 (mapc
  (lambda (item)
-   (add-to-list 'package-archives item))
- '(("ELPA"      . "http://tromey.com/elpa/")
-   ("marmalade" . "http://marmalade-repo.org/packages/")))
+   (add-to-list 'package-archives item 'append))
+ '(("marmalade" . "http://marmalade-repo.org/packages/")
+   ("melpa" . "http://melpa.milkbox.net/packages/")))
