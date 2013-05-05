0165e922ec53daa078d87561d0481a67cc7acf2c
Victor Deryagin <vderyagin@gmail.com>
2013-05-05 17:46:47 +0300

    removed few more eshell customizations

--- conf/eshell-configuration.el
+++ conf/eshell-configuration.el
@@ -2,13 +2,14 @@
 
 (custom-set-variables
  '(eshell-directory-name "~/.emacs.d/eshell/")
- '(eshell-cmpl-cycle-completions nil)
- '(eshell-save-history-on-exit t)
  '(eshell-scroll-show-maximum-output nil)
  '(eshell-scroll-to-bottom-on-input 'this)
  '(eshell-scroll-to-bottom-on-output 'this)
  '(eshell-cmpl-dir-ignore (rx string-start
-                              (or (and "." (optional ".")) "CVS" "_darcs" (and "." (or ".svn" ".hg" ".git")))
+                              (or (and "." (or
+                                            "svn" "hg" "git"
+                                            (optional ".")))
+                                  "CVS" "_darcs")
                               "/"
                               string-end)))
 
