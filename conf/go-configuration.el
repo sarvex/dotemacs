3561e842fbcb9ec9bcd9a08bfdc372fb23e391ea
Victor Deryagin <vderyagin@gmail.com>
2013-07-22 09:42:53 +0300

    reindent

--- conf/go-configuration.el
+++ conf/go-configuration.el
@@ -1,21 +1,24 @@
 ;;; -*- lexical-binding: t -*-
 
 (eval-after-load 'go-mode
-  '(progn
-    (require 'go-autocomplete)
+  (quote
+   (progn
+     (require 'go-autocomplete)
+     (require 'go-eldoc)
 
-    (add-hook 'go-mode-hook
-     (lambda ()
-       (add-hook 'before-save-hook 'gofmt nil 'make-it-local)
-       (set (make-local-variable 'compile-command) "go test")
-       (setq tab-width 2
-             show-trailing-whitespace t)))
+     (add-hook 'go-mode-hook
+               (lambda ()
+                 (go-eldoc-setup)
+                 (add-hook 'before-save-hook 'gofmt nil 'make-it-local)
+                 (set (make-local-variable 'compile-command) "go test")
+                 (setq tab-width 2
+                       show-trailing-whitespace t)))
 
-    (define-key go-mode-map (kbd "M-i") 'go-import-add)
-    (define-key go-mode-map (kbd "C-c C-r") 'go-remove-unused-imports)
+     (define-key go-mode-map (kbd "M-i") 'go-import-add)
+     (define-key go-mode-map (kbd "C-c C-r") 'go-remove-unused-imports)
 
-    (define-key go-mode-map (kbd "M-.") 'godef-jump)
-    (define-key go-mode-map (kbd "C-?") 'godef-describe)
+     (define-key go-mode-map (kbd "M-.") 'godef-jump)
+     (define-key go-mode-map (kbd "C-?") 'godef-describe)
 
-    (define-key go-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
-    (define-key go-mode-map (kbd "C-c C-c") 'gofmt)))
+     (define-key go-mode-map (kbd "<return>") 'reindent-then-newline-and-indent)
+     (define-key go-mode-map (kbd "C-c C-c") 'gofmt))))
