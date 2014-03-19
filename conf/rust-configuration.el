87f318fab7dcc83a0715b4a3e09eef6b95f3d570
Victor Deryagin <vderyagin@gmail.com>
2014-03-19 10:38:07 +0200

    More changes to compiling/running rust stuff.

--- conf/rust-configuration.el
+++ conf/rust-configuration.el
@@ -46,24 +46,23 @@
    (rx not-newline word-boundary "test.rs" string-end)
    filename))
 
-(defun rust-compile-command ()
+(defun rust-compile-command (&optional run)
   (when buffer-file-name
-    (let ((source-file (file-name-nondirectory buffer-file-name)))
-      (format (if (rust-test-file-p source-file) "rustc --test %s" "rustc %s")
-              source-file))))
-
-(defun rust-compile-and-run-command ()
-  (let* ((compile-cmd (rust-compile-command))
-         (executable (file-name-sans-extension (file-name-nondirectory buffer-file-name))))
-    (concat compile-cmd " && ./" executable)))
+    (let* ((source-file (file-name-nondirectory buffer-file-name))
+           (executable (file-name-sans-extension source-file))
+           (cmd (format (if (rust-test-file-p source-file)
+                            "rustc --test %s"
+                          "rustc %s")
+                        source-file)))
+      (if run
+          (concat cmd " && ./" executable)
+        cmd))))
 
 (defun rust-compile-and-maybe-run (&optional force-run)
   "Compile current file, when it is a test file or when command
 is run with prefix argument - also execute resulting binary."
   (interactive "P")
   (let ((compile-command
-         (if (or force-run
-                 (rust-test-file-p buffer-file-name))
-             (rust-compile-and-run-command)
-           (rust-compile-command))))
+         (rust-compile-command (or force-run
+                                   (rust-test-file-p buffer-file-name)))))
     (recompile)))
