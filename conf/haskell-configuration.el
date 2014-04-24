1fc9444c2a74b5e3fea81ea1ec7b0393ce7550de
Victor Deryagin <vderyagin@gmail.com>
2014-04-24 12:28:38 +0300

    Not using structured-haskell-mode yet.

--- conf/haskell-configuration.el
+++ conf/haskell-configuration.el
@@ -3,7 +3,7 @@
 (eval-after-load 'haskell-mode
   '(progn
     (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-    ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-    (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+    ;; (add-hook 'haskell-mode-hook 'structured-haskell-mode)
     (add-hook 'haskell-mode-hook
-              (lambda () (setq (make-local-variable 'compile-command) "cabal test")))))
+              (lambda () (set (make-local-variable 'compile-command) "cabal test")))))
