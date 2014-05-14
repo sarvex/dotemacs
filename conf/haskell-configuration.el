3a1f121aa8c0fd4b166d89f93a61812db448b0c6
Victor Deryagin <vderyagin@gmail.com>
2014-05-14 17:39:47 +0300

    Some haskell-mode customization.

--- conf/haskell-configuration.el
+++ conf/haskell-configuration.el
@@ -1,9 +1,33 @@
 ;;; -*- lexical-binding: t -*-
 
 (eval-after-load 'haskell-mode
-  '(progn
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-    ;; (add-hook 'haskell-mode-hook 'structured-haskell-mode)
-    (add-hook 'haskell-mode-hook
-              (lambda () (set (make-local-variable 'compile-command) "cabal test")))))
+  (quote
+   (progn
+     (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+     (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+     ;; (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+
+     (add-hook 'haskell-mode-hook
+               (lambda () (set (make-local-variable 'compile-command) "cabal test")))
+
+     (define-key haskell-mode-map (kbd "<f8>") 'haskell-navigate-imports)
+
+     (require 'align)
+
+     (add-to-list 'align-rules-list
+                  '(haskell-types
+                    (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
+                    (modes quote (haskell-mode literate-haskell-mode))))
+     (add-to-list 'align-rules-list
+                  '(haskell-assignment
+                    (regexp . "\\(\\s-+\\)=\\s-+")
+                    (modes quote (haskell-mode literate-haskell-mode))))
+     (add-to-list 'align-rules-list
+                  '(haskell-arrows
+                    (regexp . "\\(\\s-+\\)\\(->\\|→\\)\\s-+")
+                    (modes quote (haskell-mode literate-haskell-mode))))
+     (add-to-list 'align-rules-list
+                  '(haskell-left-arrows
+                    (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+")
+                    (modes quote (haskell-mode literate-haskell-mode))))
+     )))
