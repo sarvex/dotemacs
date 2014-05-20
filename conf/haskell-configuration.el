3330b8b19ff0d8b8ca153238674b07ff9f82ecce
Victor Deryagin <vderyagin@gmail.com>
2014-05-20 15:01:53 +0300

    Try 'structured-haskell-mode' one more time.

--- conf/haskell-configuration.el
+++ conf/haskell-configuration.el
@@ -5,8 +5,8 @@
    (progn
 
      (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-     (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-     ;; (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+     ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+     (add-hook 'haskell-mode-hook 'structured-haskell-mode)
 
      (add-hook 'haskell-mode-hook
                (lambda () (set (make-local-variable 'compile-command) "cabal test")))
