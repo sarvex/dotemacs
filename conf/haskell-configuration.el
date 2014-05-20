4132729bb8b9b18c796ed7a3a6363c6ae215ce47
Victor Deryagin <vderyagin@gmail.com>
2014-05-20 15:03:07 +0300

    Set up 'haskell-process' stuff.

--- conf/haskell-configuration.el
+++ conf/haskell-configuration.el
@@ -1,5 +1,12 @@
 ;;; -*- lexical-binding: t -*-
 
+(custom-set-variables
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-type cabal-repl)
+ )
+
 (eval-after-load 'haskell-mode
   (quote
    (progn
@@ -11,6 +18,28 @@
      (add-hook 'haskell-mode-hook
                (lambda () (set (make-local-variable 'compile-command) "cabal test")))
 
+     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+     (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+     (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+     (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+     (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+     (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+     (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+     (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+
      (define-key haskell-mode-map (kbd "<f8>") 'haskell-navigate-imports)
+
+     )))
+
+
+(eval-after-load 'haskell-cabal
+  (quote
+   (progn
+
+     (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+     (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+     (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+     (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
 
      )))
