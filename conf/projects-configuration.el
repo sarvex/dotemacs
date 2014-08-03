8b30c67eddbfbbf0a3620fad2e66dd259f1b788d
Victor Deryagin <vderyagin@gmail.com>
2014-08-03 11:05:23 +0300

    'pe/omit-regex'.

--- conf/projects-configuration.el
+++ conf/projects-configuration.el
@@ -1,3 +1,16 @@
 ;;; -*- lexical-binding: t -*-
 
 (define-key global-map (kbd "C-c f") 'projectur-find-file)
+
+(custom-set-variables
+ '(pe/omit-regex (rx (or (and string-start "#")
+                         (and "~" string-end)
+                         (and ".elc" string-end)
+                         (and string-start
+                              (or (and ".git" (optional "keep"))
+                                  "dist"
+                                  "node_modules"
+                                  "tmp"
+                                  "vendor"
+                                  )
+                              string-end)))))
