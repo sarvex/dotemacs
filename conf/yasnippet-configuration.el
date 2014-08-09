6cb3f080959ca777a50119d90c8521e349d6f676
Victor Deryagin <vderyagin@gmail.com>
2014-08-09 11:51:37 +0300

    Customize yasnippet.

--- conf/yasnippet-configuration.el
+++ conf/yasnippet-configuration.el
@@ -32,8 +32,24 @@
 (eval-after-load 'yasnippet
   (quote
    (progn
+
+     (defun yas-activate-if-extension (mode file-name &rest exts)
+       (when (and file-name
+                  (string-match-p
+                   (rx-to-string `(and not-newline "." (eval (list 'or ,@exts)) string-end))
+                   file-name))
+         (yas-activate-extra-mode mode)))
+
+     (defun yas-activate-extra-modes-based-on-file-name ()
+           (yas-activate-if-extension 'handlebars-mode buffer-file-name "hbs" "handlebars")
+           (yas-activate-if-extension 'rhtml-mode buffer-file-name "rhtml" "html.erb"))
+
+     (add-hook 'yas-minor-mode-hook 'yas-activate-extra-modes-based-on-file-name)
+
      (add-hook 'snippet-mode-hook
                (lambda ()
                  (setq require-final-newline nil)
                  (add-hook 'after-save-hook 'yas-recompile-all 'append 'make-it-local)
-                 (add-hook 'after-save-hook 'yas-reload-all 'append 'make-it-local))))))
+                 (add-hook 'after-save-hook 'yas-reload-all 'append 'make-it-local)))
+
+     )))
