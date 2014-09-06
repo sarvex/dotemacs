d3f284f425203ebf37ebc4d8e5b2c6297125979f
Victor Deryagin <vderyagin@gmail.com>
2014-09-06 18:46:15 +0300

    move functions to top-level

--- conf/yasnippet-configuration.el
+++ conf/yasnippet-configuration.el
@@ -29,20 +29,20 @@
 
 (add-to-list 'auto-mode-alist '("\\.yasnippet\\'" . snippet-mode))
 
+(defun yas-activate-if-extension (mode file-name &rest exts)
+  (when (and file-name
+             (string-match-p
+              (rx-to-string `(and not-newline "." (eval (list 'or ,@exts)) string-end))
+              file-name))
+    (yas-activate-extra-mode mode)))
+
+(defun yas-activate-extra-modes-based-on-file-name ()
+  (yas-activate-if-extension 'handlebars-mode buffer-file-name "hbs" "handlebars")
+  (yas-activate-if-extension 'rhtml-mode buffer-file-name "rhtml" "html.erb"))
+
 (eval-after-load 'yasnippet
   (quote
    (progn
-
-     (defun yas-activate-if-extension (mode file-name &rest exts)
-       (when (and file-name
-                  (string-match-p
-                   (rx-to-string `(and not-newline "." (eval (list 'or ,@exts)) string-end))
-                   file-name))
-         (yas-activate-extra-mode mode)))
-
-     (defun yas-activate-extra-modes-based-on-file-name ()
-           (yas-activate-if-extension 'handlebars-mode buffer-file-name "hbs" "handlebars")
-           (yas-activate-if-extension 'rhtml-mode buffer-file-name "rhtml" "html.erb"))
 
      (add-hook 'yas-minor-mode-hook 'yas-activate-extra-modes-based-on-file-name)
 
