20d8b7b1d34219ed11e108989305cf7a80ab5c08
Victor Deryagin <vderyagin@gmail.com>
2014-10-01 22:06:55 +0300

    display bookmarks instead of *scratch* buffer.

--- conf/misc-configuration.el
+++ conf/misc-configuration.el
@@ -177,6 +177,7 @@
  '(indicate-buffer-boundaries 'left)
  '(indicate-empty-lines t)
  '(inhibit-startup-screen t)
+ '(initial-buffer-choice (lambda (list-bookmarks) (get-buffer "*Bookmark List*")))
  '(initial-scratch-message "")
  '(large-file-warning-threshold nil)
  '(mouse-drag-copy-region nil)
