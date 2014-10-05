b21f5a03f534800e06d111ebc2d3c080be7dfced
Victor Deryagin <vderyagin@gmail.com>
2014-10-05 12:02:12 +0300

    colorize 'shell-command' output.

--- conf/misc-configuration.el
+++ conf/misc-configuration.el
@@ -3,6 +3,14 @@
 (require 'uniquify)
 (require 'ansi-color)
 
+(defadvice display-message-or-buffer (before ansi-color activate)
+  "Process ANSI color codes in shell output."
+  (let ((buf (ad-get-arg 0)))
+    (and (bufferp buf)
+         (string= (buffer-name buf) "*Shell Command Output*")
+         (with-current-buffer buf
+           (ansi-color-apply-on-region (point-min) (point-max))))))
+
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (random t)
