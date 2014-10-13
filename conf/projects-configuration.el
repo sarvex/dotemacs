e883cc0cede4880e51e778616c0f5d144fab7216
Victor Deryagin <vderyagin@gmail.com>
2014-10-13 12:26:10 +0300

    Ensure setting pe/omit-regex.

--- conf/projects-configuration.el
+++ conf/projects-configuration.el
@@ -2,25 +2,25 @@
 
 (define-key global-map (kbd "C-c f") 'projectur-find-file)
 
-(custom-set-variables
- '(pe/omit-regex (rx (or (and string-start "#")
-                         (and (or "!"
-                                  ".elc"
-                                  ".test"
-                                  )
-                              string-end)
-                         (and string-start
-                              (or (and ".git" (optional "keep"))
-                                  ".module-cache"
-                                  "bower_components"
-                                  "dist"
-                                  "node_modules"
-                                  "tmp"
-                                  "vendor"
-                                  )
-                              string-end)))))
-
 (with-eval-after-load 'project-explorer
+  (setq pe/omit-regex
+        (rx (or (and string-start "#")
+                (and (or "!"
+                         ".elc"
+                         ".test"
+                         )
+                     string-end)
+                (and string-start
+                     (or (and ".git" (optional "keep"))
+                         ".module-cache"
+                         "bower_components"
+                         "dist"
+                         "node_modules"
+                         "tmp"
+                         "vendor"
+                         )
+                     string-end))))
+
   (define-key project-explorer-mode-map (kbd "o")
     (lambda ()
       (interactive)
