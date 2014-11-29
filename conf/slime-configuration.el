43aa5f2a2f1b4321ab70155704ddeb62a9f8bfda
Victor Deryagin <vderyagin@gmail.com>
2014-11-29 06:17:24 +0200

    slime: clean up setup.

--- conf/slime-configuration.el
+++ conf/slime-configuration.el
@@ -1,20 +1,10 @@
 ;;; -*- lexical-binding: t -*-
 
-(setq inferior-lisp-program "/usr/bin/sbcl --noinform --core /usr/lib/sbcl/sbcl.core"
-      common-lisp-hyperspec-root "file:/usr/share/doc/hyperspec/HyperSpec/")
-
 (require 'slime-autoloads)
 
-(with-eval-after-load 'slime
-  (require 'paredit)
-  (slime-setup '(slime-fancy))
-
-  (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
-
-  ;; Stop SLIME's REPL from grabbing DEL, which is annoying when backspacing over a '('
-  (define-key slime-repl-mode-map (read-kbd-macro paredit-backward-delete-key) nil))
-
 (custom-set-variables
+ '(common-lisp-hyperspec-root "file:/usr/share/doc/hyperspec/HyperSpec/")
+ '(inferior-lisp-program "/usr/bin/sbcl --noinform --core /usr/lib/sbcl/sbcl.core")
  '(slime-autodoc-use-multiline-p t)
  '(slime-header-line-p nil)
  '(slime-net-coding-system 'utf-8-unix)
@@ -22,4 +12,8 @@
  '(slime-repl-history-remove-duplicates t)
  '(slime-repl-history-size 2000)
  '(slime-repl-history-trim-whitespaces t)
- '(slime-startup-animation nil))
+ '(slime-startup-animation nil)
+ )
+
+(with-eval-after-load 'slime
+  (slime-setup '(slime-fancy)))
