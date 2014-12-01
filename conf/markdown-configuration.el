d82b1f4bf57c4efc95ecb1bf28f6911935bb0309
Victor Deryagin <vderyagin@gmail.com>
2014-12-01 08:48:53 +0200

    markdown: no autofill.

--- conf/markdown-configuration.el
+++ conf/markdown-configuration.el
@@ -3,10 +3,8 @@
  '(markdown-coding-system 'utf-8))
 
 (with-eval-after-load 'markdown-mode
-  (add-hook 'markdown-mode-hook 'turn-on-auto-fill)
   (add-hook 'markdown-mode-hook 'turn-on-orgtbl)
   (add-hook 'markdown-mode-hook 'yas-minor-mode-on)
-  (add-hook 'markdown-mode-hook (lambda () (set-fill-column 68)))
 
   (define-key markdown-mode-map (kbd "<f9>") 'markdown-other-window)
 
